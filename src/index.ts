5509a17a47bff39ff4f5dababc5d59beec60e7b3
zhangchao <nezhaat78@gmail.com>
2022-06-23 16:45:58 +0800

    Update index.ts
    add json string parse

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,16 @@ function getNestedObject(nestedObj: any, pathArr: string[]) {
 
 async function run() {
   const path: string = core.getInput('path');
+  const jsonString: string = core.getInput('json');
   const prop: string[] = core.getInput('prop_path').split('.');
   try {
-    const buffer = await readFileAsync(path);
-    const json = JSON.parse(buffer.toString());
+    var json: string;
+    if(path){
+      const buffer = await readFileAsync(path); 
+      json = JSON.parse(buffer.toString());
+    } else {
+      json = JSON.parse(jsonString);
+    }
     const nestedProp = getNestedObject(json, prop);
     if (nestedProp) {
       core.setOutput('prop', nestedProp);
